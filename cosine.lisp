6f8eb952789f2e544034eab9fe9f39f9e323608e
Sergio Gonzalez <bigmonachus@gmail.com>
2013-11-30 13:49:30 -0600

    cosine fitness

--- cosine.lisp
+++ cosine.lisp
@@ -4,12 +4,29 @@
 ;; SBCL:
 ;; (ql:quickload "swank")
 ;; (swank:start-server)
-
+;(proclaim '(optimize (speed 3) (space 0) (safety 0)))
 
 (defun run (fitness-func population)
   (loop for m in population collect
-        (progn
-          (fitness-func m))))
+        (funcall fitness-func m)))
 
-(defun cosine-fitness (f x)
-  (abs(- (cos x) (f x))))
+(defun cosine-fitness (func)
+  (declare (optimize (speed 3) (safety 0) (space 0) (debug 0)))
+  (let ((sum 0.0))
+    (declare (type float sum))
+    (loop for x float from 0.0 below 3.1416 by 0.1
+          do (let ((res (funcall func x)))
+               (declare (type float res))
+               (setf sum (+ sum
+                            (abs (- (cos x) res))))))
+    sum))
+
+(cosine-fitness #'cos)
+
+(let ((functions (list #'(lambda (x) x) #'cos)))
+  (mapcar #'list
+          functions
+          (run #'cosine-fitness functions)))
+
+(defun my-make ()
+  )
