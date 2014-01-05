b2bf17e6f1915189572b5659207d1d09c4b31ddb
Sergio Gonzalez <bigmonachus@gmail.com>
2014-01-04 20:19:45 -0600

    tweaks en vacaciones

--- sine.py
+++ sine.py
@@ -2,11 +2,14 @@ from pyevolve import Util
 from pyevolve import GTree
 from pyevolve import GSimpleGA
 from pyevolve import Consts
+from pyevolve import Selectors
 import math
 
 rmse_accum = Util.ErrorAccumulator()
 
-def gp_add(a, b): return a+b
+def gp_add2(a, b): return a+b
+def gp_add3(a, b, c): return a+b+c
+# def gp_add4(a, b, c, d): return a+b+c+d
 def gp_sub(a, b): return a-b
 def gp_mul(a, b): return a*b
 def gp_sqrt(a):   return math.sqrt(abs(a))
@@ -16,32 +19,36 @@ def eval_func(chromosome):
     rmse_accum.reset()
     code_comp = chromosome.getCompiledCode()
     a = 0
-    while a <= 2 * math.pi:
+    pi = math.pi
+    xs = [math.pi * i / 8.0 for i in xrange(0,9)]
+    for a in xs:
         evaluated     = eval(code_comp)
         target        = math.sin(a)
         rmse_accum   += (target, evaluated)
-        a += 0.1
 
     return rmse_accum.getRMSE()
 
 def main_run():
-   genome = GTree.GTreeGP()
-   genome.setParams(max_depth=4, method="ramped")
-   genome.evaluator += eval_func
+    genome = GTree.GTreeGP()
+    genome.setParams(max_depth=10, method="ramped")
+    genome.evaluator += eval_func
 
-   ga = GSimpleGA.GSimpleGA(genome)
-   ga.setParams(gp_terminals = ['a', '0.5', '0.1', '1', '2', '3', '4', '5', '6'],
-                gp_function_prefix = "gp")
+    ga = GSimpleGA.GSimpleGA(genome)
+    # ga.selector.set(Selectors.GTournamentSelector)
+    terminals = ['a', '0.1', '0.5', '1', '2', str(math.pi)] #, '1', '2', str(math.pi)]
 
-   ga.setMinimax(Consts.minimaxType["minimize"])
-   ga.setGenerations(50)
-   ga.setCrossoverRate(0.5)
-   ga.setMutationRate(0.15)
-   ga.setPopulationSize(800)
+    ga.setParams(gp_terminals = terminals,
+                 gp_function_prefix = "gp")
 
-   ga(freq_stats=10)
-   best = ga.bestIndividual()
-   print best
+    ga.setMinimax(Consts.minimaxType["minimize"])
+    ga.setGenerations(500)
+    ga.setCrossoverRate(1.0)
+    ga.setMutationRate(0.1)
+    ga.setPopulationSize(200)
+
+    ga(freq_stats=100)
+    best = ga.bestIndividual()
+    print best
 
 if __name__ == "__main__":
-   main_run()
+    main_run()
